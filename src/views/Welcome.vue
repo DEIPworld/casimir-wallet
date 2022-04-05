198c69cda81b35f48aa7873be9ad76f881673783
morfmorf <m0rfik@mail.ru>
2022-04-05 11:25:46 +0100

    Added layout components

---
+++ src/views/Welcome.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // Lib
+  import { defineComponent } from 'vue';
+
+  // Components
+  import Container from '@/components/Container.vue';
+  import Button from '@/components/Button.vue';
+
+  export default defineComponent({
+    components: {
+      Container,
+      Button,
+    },
+  });
+</script>
+
+<template>
+  <Container class-name="container--black">
+    <Button>
+      Create an account
+    </Button>
+    <Button class-name="button--transparent">
+      Import an existing account
+    </Button>
+  </Container>
+</template>
