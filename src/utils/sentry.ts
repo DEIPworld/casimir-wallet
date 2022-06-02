e631a1dea77f0b588fac66116190b369fcf565bf
morfmorf <m0rfik@mail.ru>
2022-06-02 14:52:27 +0300

    Added sentry release and env

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -11,13 +11,15 @@ const SentryInit = (app: App, router: Router) => Sentry.init({
     new BrowserTracing({
       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
       tracingOrigins: [
-        "wallet-testnet.deip.world",
-        "wallet.deip.world",
+        'wallet-testnet.deip.world',
+        'wallet.deip.world',
         /^\//
       ]
     })
   ],
-  tracesSampleRate: import.meta.env.PROD ? 1.0 : undefined
+  tracesSampleRate: import.meta.env.PROD ? 1.0 : 0.2,
+  environment: import.meta.env.MODE,
+  release: import.meta.env.VITE_RELEASE
 });
 
 export default SentryInit;
