96f159657531b29deaaa651a77104c26e55da4a7
Igor Novitsky <ionovitsky@gmail.com>
2022-06-23 18:17:22 +0300

    chore: Get mulstisig wallet data on top route level

--- src/components/MultisigView/MultisigView.tsx
+++ src/components/MultisigView/MultisigView.tsx
@@ -1,19 +1,42 @@
-import { defineComponent, watchEffect } from 'vue';
+import { defineComponent, onBeforeMount, onBeforeUnmount, watch, toRef } from 'vue';
 import { RouterView } from 'vue-router';
 
+import { useMultisigWalletStore } from '@/stores/multisigWallet';
 import { useAccountStore } from '@/stores/account';
 
 export const MultisigView = defineComponent({
   props: {
     address: {
-      type: String
+      type: String,
+      required: true
     }
   },
   setup(props) {
+    const multisigStore = useMultisigWalletStore();
+    const address = toRef(props, 'address');
     const accountStore = useAccountStore();
 
-    watchEffect(() => {
-      if (props.address) accountStore.getMultisigAccountDetails(props.address);
+    onBeforeMount(async () => {
+      accountStore.getMultisigAccountDetails(props.address);
+      multisigStore.subscribeToTransfers(props.address);
+      multisigStore.getAccountBalance(props.address);
+      multisigStore.getPendingApprovals(props.address);
+    });
+
+    onBeforeUnmount(() => {
+      multisigStore.clear();
+      multisigStore.unsubscribeFromTransfers(props.address);
+    });
+
+    watch(address, (currentAddress, prevAddress) => {
+      multisigStore.clear();
+
+      accountStore.getMultisigAccountDetails(currentAddress);
+      multisigStore.subscribeToTransfers(currentAddress);
+      multisigStore.getAccountBalance(currentAddress);
+      multisigStore.getPendingApprovals(currentAddress);
+
+      multisigStore.unsubscribeFromTransfers(prevAddress);
     });
 
     return () => (
