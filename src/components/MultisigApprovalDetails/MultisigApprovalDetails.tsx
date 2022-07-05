45444bb7b388b3c19957018ddccff11f06e700b3
Igor Novitsky <ionovitsky@gmail.com>
2022-07-05 15:09:12 +0300

    fix: Add approval type validation

--- src/components/MultisigApprovalDetails/MultisigApprovalDetails.tsx
+++ src/components/MultisigApprovalDetails/MultisigApprovalDetails.tsx
@@ -1,7 +1,14 @@
 import { defineComponent } from 'vue';
 
+import type { Component } from 'vue';
+
 import { MultisigApprovalDetailsTransfer } from './MultisigApprovalDetailsTransfer';
 import { MultisigApprovalDetailsVesting } from './MultisigApprovalDetailsVesting';
+
+const componentMap: Component = {
+  'transfer': MultisigApprovalDetailsTransfer,
+  'vesting': MultisigApprovalDetailsVesting
+};
 
 export const MultisigApprovalDetails = defineComponent({
   props: {
@@ -11,18 +18,19 @@ export const MultisigApprovalDetails = defineComponent({
     },
     approvalType: {
       type: String,
-      required: true
+      required: true,
+      validator(value: string) {
+        return ['transfer', 'vesting'].includes(value);
+      }
     }
   },
   setup(props) {
-    const renderDetailsView = () => {
-      if (props.approvalType === 'transfer') {
-        return <MultisigApprovalDetailsTransfer approvalId={props.approvalId} />;
-      }
+    const Component = componentMap[props.approvalType];
 
-      return < MultisigApprovalDetailsVesting approvalId={props.approvalId} />;
-    };
+    if (!Component) {
+      return () => <div class="text-center text-subtitle-1">Invalid approval type</div>;
+    }
 
-    return renderDetailsView;
+    return () => <Component approvalId={props.approvalId} />;
   }
 });
