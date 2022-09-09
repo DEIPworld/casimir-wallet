1ada66c6b728b4158fe29980a2fffb7c9fd74873
egortsaryk9 <egortsaryk@gmail.com>
2022-09-09 23:41:33 +0200

    node connection string

--- build-docker.sh
+++ build-docker.sh
@@ -7,12 +7,13 @@ ORG="deipworld"
 IMAGE_PREFIX="$1"
 DW_API_URL="$2"
 DW_SENTRY="$3"
+DW_NETWORK="$4"
 DW_RELEASE=$(git show -s --format=%H)
 
 echo "Building $ORG/$IMAGE_PREFIX-web-wallet image..."
 export IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-wallet:$TAG"
 export LATEST_IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-wallet:$LATEST"
-docker build --build-arg DW_API_URL=${DW_API_URL} --build-arg DW_SENTRY=${DW_SENTRY} -t="${IMAGE_NAME}" -f=Dockerfile.dev .
+docker build --build-arg DW_API_URL=${DW_API_URL} --build-arg DW_SENTRY=${DW_SENTRY} --build-arg DW_NETWORK=${DW_NETWORK} -t="${IMAGE_NAME}" -f=Dockerfile.dev .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
 docker push ${IMAGE_NAME}
 docker push ${LATEST_IMAGE_NAME}
