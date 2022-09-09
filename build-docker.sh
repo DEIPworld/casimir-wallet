c2dff2e0ab2a21b7c39d3e12764f92e7e43fdb98
egortsaryk9 <egortsaryk@gmail.com>
2022-09-10 00:48:41 +0200

    Dockerfile cache fixed

--- build-docker.sh
+++ build-docker.sh
@@ -13,7 +13,7 @@ DW_RELEASE=$(git show -s --format=%H)
 echo "Building $ORG/$IMAGE_PREFIX-web-wallet image..."
 export IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-wallet:$TAG"
 export LATEST_IMAGE_NAME="$ORG/$IMAGE_PREFIX-web-wallet:$LATEST"
-docker build --build-arg DW_API_URL=${DW_API_URL} --build-arg DW_SENTRY=${DW_SENTRY} --build-arg DW_NETWORK=${DW_NETWORK} -t="${IMAGE_NAME}" -f=Dockerfile.stage .
+docker build --build-arg DW_API_URL=${DW_API_URL} --build-arg DW_SENTRY=${DW_SENTRY} --build-arg DW_RELEASE=${DW_RELEASE} --build-arg DW_NETWORK=${DW_NETWORK} -t="${IMAGE_NAME}" -f=Dockerfile.stage .
 docker tag ${IMAGE_NAME} ${LATEST_IMAGE_NAME}
 docker push ${IMAGE_NAME}
 docker push ${LATEST_IMAGE_NAME}
